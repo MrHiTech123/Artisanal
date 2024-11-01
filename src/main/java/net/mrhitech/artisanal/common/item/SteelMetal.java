4aa438bdc76cab6bcd8bbb62ec91feebc3ecc15a
MrHiTech <mlebowitz97@gmail.com>
2024-11-01 13:29:39 -0400

    Gave flints and steels appropriate rarities

--- src/main/java/net/mrhitech/artisanal/common/item/SteelMetal.java
+++ src/main/java/net/mrhitech/artisanal/common/item/SteelMetal.java
@@ -1,25 +1,31 @@
 package net.mrhitech.artisanal.common.item;
 
-import net.dries007.tfc.common.TFCTiers;
+import net.dries007.tfc.util.Metal;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.Tier;
 
 import java.util.Locale;
 
 public enum SteelMetal {
-    STEEL(TFCTiers.STEEL),
-    BLACK_STEEL(TFCTiers.BLACK_STEEL),
-    BLUE_STEEL(TFCTiers.RED_STEEL),
-    RED_STEEL(TFCTiers.BLUE_STEEL);
+    STEEL(Metal.Default.STEEL),
+    BLACK_STEEL(Metal.Default.BLACK_STEEL),
+    BLUE_STEEL(Metal.Default.RED_STEEL),
+    RED_STEEL(Metal.Default.BLUE_STEEL);
     
-    private Tier tier;
-    SteelMetal(Tier f_tier) {
-        tier = f_tier;
+    private Metal.Default metal;
+    SteelMetal(Metal.Default f_metal) {
+        metal = f_metal;
     }
     
     public String getSerializedName() {
         return this.name().toLowerCase(Locale.ROOT);
     }
     public Tier getTier() {
-        return this.tier;
+        return this.metal.toolTier();
+    }
+    
+    public Rarity getRarity() {
+        return this.metal.getRarity();
     }
 }
+
